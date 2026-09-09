fc927e1fd3bedc9683f647fb02e0dc90ad26d9b9
Barbara Pušnar <barbara.pusnar@scng.si>
2026-09-09 09:19:37 +0200

    VajeRekrzija

--- Vaja1.java
+++ Vaja1.java
@@ -1,6 +1,21 @@
 public class Vaja1
 {
     public static void main(String[] args) {
-        System.out.println("Rad imam Javo");
+        System.out.println("2 na 10 = "+potenca(10));
+        System.out.println("7 Fibb število je "+fib(7));
+    }
+    public static  int potenca(int n)
+    {
+        //izračuna 2 na n rekurzivno
+        if (n==0)
+            return 1;
+        return 2*potenca(n-1);
+    }
+    public static int fib(int n)
+    {
+        //izračuna n-to fibonaccijevo število
+        if (n==1||n==2)
+            return 1;
+        return fib(n-1)+fib(n-2);
     }
 }
